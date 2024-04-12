4f5c31d883ca81b96534536e9d7fe23dd99dab3b
Raman Harhun <r.harhun@gmail.com>
2024-04-12 08:57:10 +0300

    Push start vc

---
+++ WiFiDetector/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WiFiDetector
+//
+//  Created by Sergey Antoniuk on 12.04.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = .blue
+	}
+
+
+}
+
